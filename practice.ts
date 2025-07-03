a6050d01e9570a4f81acee364b88eb2a8f983608
lumijoe <spacedesign63@gmail.com>
2025-07-03 10:04:37 +0900

    ate

---
+++ practice.ts
@@ -0,0 +1,13 @@
+class Cat {
+    meow() {/* 鳴メソッドの定義 */}
+    angry() {/* 怒るメソッドの定義 */}
+}
+
+const myCat = new Cat();
+myCat.meow();  //=>にゃー
+myCat.angry(); //=>フガー！
+
+
+let price: number = 150;
+let fruits: string = "りんご";
+price = 170;
